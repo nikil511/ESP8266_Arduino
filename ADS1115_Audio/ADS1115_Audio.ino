bd9402a56a539fa3f8d8d8a68b93ee0e8dee95dd
Manolis N <nikil511@gmail.com>
2016-07-29 16:46:17 +0300

    Audio Level sampling via ESP8266

---
+++ ADS1115_Audio/ADS1115_Audio.ino
@@ -0,0 +1,83 @@
+#include <Wire.h>
+#include <Adafruit_ADS1015.h>
+
+Adafruit_ADS1115 ads;  /* Use this for the 16-bit version */
+// Adafruit_ADS1015 ads;     /* Use thi for the 12-bit version */
+
+int16_t adc0, adc1, adc2, adc3;
+const int sampleWindow = 500; // Sample window width in mS (50 mS = 20Hz)
+unsigned int sample;
+unsigned int sampleCnt;
+unsigned long adcT0;
+void setup(void)
+{
+  Serial.begin(115200);
+  Serial.println("Hello!");
+
+  Serial.println("Getting single-ended readings from AIN0..3");
+  Serial.println("ADC Range: +/- 6.144V (1 bit = 3mV/ADS1015, 0.1875mV/ADS1115)");
+
+  // The ADC input range (or gain) can be changed via the following
+  // functions, but be careful never to exceed VDD +0.3V max, or to
+  // exceed the upper and lower limits if you adjust the input range!
+  // Setting these values incorrectly may destroy your ADC!
+  //                                                                ADS1015  ADS1115
+  //                                                                -------  -------
+  // ads.setGain(GAIN_TWOTHIRDS);  // 2/3x gain +/- 6.144V  1 bit = 3mV      0.1875mV (default)
+  // ads.setGain(GAIN_ONE);        // 1x gain   +/- 4.096V  1 bit = 2mV      0.125mV
+     ads.setGain(GAIN_TWO);        // 2x gain   +/- 2.048V  1 bit = 1mV      0.0625mV
+  // ads.setGain(GAIN_FOUR);       // 4x gain   +/- 1.024V  1 bit = 0.5mV    0.03125mV
+  // ads.setGain(GAIN_EIGHT);      // 8x gain   +/- 0.512V  1 bit = 0.25mV   0.015625mV
+  // ads.setGain(GAIN_SIXTEEN);    // 16x gain  +/- 0.256V  1 bit = 0.125mV  0.0078125mV
+
+  ads.begin();
+
+
+  adc0 = ads.readADC_SingleEnded(0);
+  adc1 = ads.readADC_SingleEnded(1);
+  adc2 = ads.readADC_SingleEnded(2);
+  adc3 = ads.readADC_SingleEnded(3);
+  Serial.print("AIN0: "); Serial.println(adc0);
+  Serial.print("AIN1: "); Serial.println(adc1);
+  Serial.print("AIN2: "); Serial.println(adc2);
+  Serial.print("AIN3: "); Serial.println(adc3);
+  Serial.println(" ");
+
+}
+
+void loop(void)
+{
+
+  unsigned long startMillis = millis(); // Start of sample window
+  unsigned int peakToPeak = 0;   // peak-to-peak level
+
+  unsigned int signalMax = 0;
+  unsigned int signalMin = 65536;
+  adcT0 = 0;
+  sampleCnt = 0;
+  while (millis() - startMillis < sampleWindow)
+  {
+    
+    sample = ads.readADC_SingleEnded(3);
+    if (sample < 65536)  // toss out spurious readings
+    {
+      sampleCnt++;
+      adcT0 += sample;
+      if (sample > signalMax)
+      {
+        signalMax = sample;  // save just the max levels
+      }
+      else if (sample < signalMin)
+      {
+        signalMin = sample;  // save just the min levels
+      }
+    }
+    //delay(2);
+  }
+  peakToPeak = signalMax - signalMin;  // max - min = peak-peak amplitude
+
+  Serial.print("Last AIN3: " + String(sample));
+  Serial.print(" PEAK:"  + String(peakToPeak));
+  Serial.print(" AVG:"  + String(adcT0 / sampleCnt));
+  Serial.println(" Total samples:" + String( sampleCnt));
+}
